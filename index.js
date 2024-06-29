d50c535f4a55e770f5231beb1039bc4d86f1c80e
Fen5242 <fendo5242s@gmail.com>
2024-06-28 23:17:24 -0500

    second commit

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql2');
-const crypto = require('crypto');
-const nodemailer = require('nodemailer');
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const crypto = require('crypto');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -23,101 +23,154 @@ connection.connect(error => {
     console.error('Error connecting to the database:', error);
     return;
   }
-  console.log('Connected to the database successfully');
+  console.log('Connected to the database');
 });
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: 'sanchezfsb3@gmail.com',
-    pass: 'ameb sqso anmt tatr'
+    pass: 'ameb sqso anmt tatr' // Usa una contraseña de aplicación aquí
   }
 });
 
-// Register User
-app.post('/api/users/register', async (req, res) => {
+// Registro de usuarios
+app.post('/api/users', async (req, res) => {
   const { username, email, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const query = `INSERT INTO Users (username, email, password) VALUES (?, ?, ?)`;
-
-  connection.query(query, [username, email, hashedPassword], (error, results) => {
-    if (error) {
-      console.error('Error inserting user:', error);
-      return res.status(500).json({ error: 'Error inserting user' });
-    }
-    res.status(201).json({ id: results.insertId, username, email });
-  });
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const query = `
+      INSERT INTO Users (username, email, password)
+      VALUES (?, ?, ?);
+    `;
+    connection.query(query, [username, email, hashedPassword], (error, results) => {
+      if (error) {
+        console.error('Error inserting user:', error);
+        res.status(500).json({ error: 'Error inserting user' });
+        return;
+      }
+      res.status(201).json({ id: results.insertId, username, email });
+    });
+  } catch (error) {
+    console.error('Error hashing password:', error);
+    res.status(500).json({ error: 'Error hashing password' });
+  }
 });
 
-// User Login
-app.post('/api/users/login', (req, res) => {
-  const { email, password } = req.body;
-  const query = `SELECT * FROM Users WHERE email = ?`;
-
-  connection.query(query, [email], async (error, results) => {
-    if (error) {
-      console.error('Error finding user:', error);
-      return res.status(500).json({ error: 'Error finding user' });
-    }
-
-    if (results.length === 0) {
-      return res.status(404).json({ error: 'User not found' });
-    }
-
-    const user = results[0];
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(401).json({ error: 'Incorrect password' });
-    }
-
-    res.status(200).json({ message: 'Login successful', user });
-  });
-});
-
-// Reset Password Request
-app.post('/api/reset-password', (req, res) => {
-  const { email } = req.body;
-  const query = 'SELECT * FROM Users WHERE email = ?';
-
-  connection.query(query, [email], (error, results) => {
-    if (error) {
-      console.error('Database query error:', error);
-      return res.status(500).json({ error: 'Error finding user' });
-    }
-
-    if (results.length === 0) {
-      return res.status(404).json({ error: 'No user found with that email address' });
-    }
-
-    const user = results[0];
-    const token = crypto.randomBytes(20).toString('hex');
-
-    const updateQuery = 'UPDATE Users SET resetPasswordToken = ?, resetPasswordExpires = ? WHERE email = ?';
-    connection.query(updateQuery, [token, Date.now() + 3600000, email], (updateError, updateResults) => {
-      if (updateError) {
-        console.error('Error updating user with reset token:', updateError);
-        return res.status(500).json({ error: 'Error updating user with reset token' });
+// Login de usuarios
+app.post('/api/users/login', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const query = 'SELECT * FROM Users WHERE username = ?';
+    connection.query(query, [username], async (error, results) => {
+      if (error) {
+        console.error('Error finding user:', error);
+        res.status(500).json({ error: 'Error finding user' });
+        return;
       }
 
-      const mailOptions = {
-        to: email,
-        from: 'your-email@gmail.com',
-        subject: 'Password Reset',
-        text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.
-               Please use the following token for resetting your password: ${token}`
-      };
+      if (results.length === 0) {
+        res.status(401).json({ error: 'User not found' });
+        return;
+      }
 
-      transporter.sendMail(mailOptions, (mailError, info) => {
-        if (mailError) {
-          console.error('Error sending mail:', mailError);
-          return res.status(500).json({ error: 'Error sending reset email' });
-        }
-        res.status(200).json({ message: 'Reset email sent successfully' });
-      });
+      const user = results[0];
+      const match = await bcrypt.compare(password, user.password);
+      if (match) {
+        res.status(200).json({
+          message: `Welcome, ${user.username}`,
+          user: {
+            id: user.id,
+            username: user.username,
+            email: user.email
+          }
+        });
+      } else {
+        res.status(401).json({ error: 'Incorrect password' });
+      }
+    });
+  } catch (error) {
+    console.error('Error during login:', error);
+    res.status(500).json({ error: 'Error during login' });
+  }
+});
+
+// Solicitud de restablecimiento de contraseña
+app.post('/api/reset-password', (req, res) => {
+  const { email } = req.body;
+  const token = crypto.randomBytes(20).toString('hex');
+
+  const query = 'UPDATE Users SET resetPasswordToken = ?, resetPasswordExpires = ? WHERE email = ?';
+  const values = [token, Date.now() + 3600000, email]; // 1 hora
+
+  connection.query(query, values, (error, results) => {
+    if (error) {
+      console.error('Error updating user with reset token:', error);
+      res.status(500).json({ error: 'Error updating user with reset token' });
+      return;
+    }
+
+    if (results.affectedRows === 0) {
+      res.status(404).json({ error: 'No user found with that email address' });
+      return;
+    }
+
+    const mailOptions = {
+      to: email,
+      from: 'kusitour.app@gmail.com',
+      subject: 'Restablecimiento de Contraseña',
+      text: `Has recibido este correo porque tú (u otra persona) ha solicitado el restablecimiento de la contraseña para tu cuenta.\n\n
+             Por favor haz clic en el siguiente enlace, o copia y pega esta dirección en tu navegador para completar el proceso:\n\n
+             https://kusitour-api.up.railway.app/reset/${token}\n\n
+             Si no solicitaste esto, por favor ignora este correo y tu contraseña permanecerá sin cambios.\n`
+    };
+
+    transporter.sendMail(mailOptions, (error, response) => {
+      if (error) {
+        console.error('Error sending reset email:', error);
+        res.status(500).json({ error: 'Error sending reset email' });
+        return;
+      }
+      console.log('Reset email sent successfully:', response);
+      res.status(200).json({ message: 'Correo de restablecimiento enviado exitosamente' });
+    });
+  });
+});
+
+// Restablecimiento de contraseña
+app.post('/api/reset/:token', async (req, res) => {
+  const { password } = req.body;
+  const token = req.params.token;
+
+  const query = 'SELECT * FROM Users WHERE resetPasswordToken = ? AND resetPasswordExpires > ?';
+  connection.query(query, [token, Date.now()], async (error, results) => {
+    if (error) {
+      console.error('Error finding user with token:', error);
+      res.status(500).json({ error: 'Error finding user with token' });
+      return;
+    }
+
+    if (results.length === 0) {
+      res.status(400).json({ error: 'Password reset token is invalid or has expired' });
+      return;
+    }
+
+    const user = results[0];
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const updateQuery = 'UPDATE Users SET password = ?, resetPasswordToken = NULL, resetPasswordExpires = NULL WHERE id = ?';
+    connection.query(updateQuery, [hashedPassword, user.id], (updateError, updateResults) => {
+      if (updateError) {
+        console.error('Error updating password:', updateError);
+        res.status(500).json({ error: 'Error updating password' });
+        return;
+      }
+      console.log('Password updated successfully:', updateResults);
+      res.status(200).json({ message: 'Contraseña actualizada exitosamente' });
     });
   });
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`HTTP Server running on port ${port}`);
 });
